f0245d7094033e2b8289742ef62e613f9aa0faf2
Alexander Morton <mortonprod@gmail.com>
2017-07-17 15:18:09 +0100

    Update home and squares

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as _ from "lodash";
+import ReactDOM from 'react-dom';
 import Clouds from "./clouds";  
 import FadeBackground from "./fadeBackground";  
 import Squares from "./squares";
@@ -11,32 +12,37 @@ import './home.css';
     @class
 */
 export default class Home extends Component {
-    squares = [];
+    squares = [null,null,null];
 	constructor(props){
 	  super(props)
-	  this.state = {yCanvas:0,isShow:[]};
-	  this.scroll = _.throttle(this.scroll.bind(this),10,{trailing:true,leading:false});
+	  this.state = {scrollTop:0,isShow:[]};
+      //this.scroll = _.throttle(this.scroll.bind(this),100,{trailing:true,leading:false});
+      window.addEventListener('scroll', this.scroll.bind(this));
 	}
     /** 
 	    Scroll event updates y position of title and if squares should move in.
+        Most scroll calculations done here. Cleaner code since we have once location for all scroll logic. 
+        Reduces the impact on performance. No point in checking if state changed before updating state.
+        Works out if squares should be exposed yet and passes scrollTop to other components for rerendering.
+        ACCESS DOM HERE!
 	    @function
     */
 	scroll(event){
 		let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-		this.setState({yCanvas:-scrollTop*0.5});
 		///
 		this.squares.map((ref,i)=>{
-		    let pos = ref.getBoundingClientRect();
-		    if(pos.top < window.innerHeight*(4/5)){
-		        let isShow = this.state.isShow;
-		        isShow[i] = true;
-		        this.setState({isShow:isShow});
-		    }else{
-		        let isShow = this.state.isShow;
-		        isShow[i] = false;
-		        this.setState({isShow:isShow});
-		    }
-
+            if(ref){
+			    let pos = ref.getBoundingClientRect();
+			    if(pos.top < window.innerHeight*(4/5)){
+			        let isShow = this.state.isShow;
+			        isShow[i] = true;
+			        this.setState({isShow:isShow,scrollTop:scrollTop});
+			    }else{
+			        let isShow = this.state.isShow;
+			        isShow[i] = false;
+			        this.setState({isShow:isShow,scrollTop:scrollTop});
+			    }
+            }
 		});
     }
     /** 
@@ -44,37 +50,32 @@ export default class Home extends Component {
 		@function
 	*/
 	componentDidMount(){
-	    window.addEventListener('scroll', this.scroll.bind(this));
 		let isShow= [];
 		for(let i=0; i < this.refs.length ; i++){
 		    isShow.push(false);
 		}
 		this.setState({isShow:isShow});
 	}
-	/** 
-	Don't update state here or repeated call to render.
-	@function
-	*/
-	addRef(ref){
-		//console.log("refs: " + this.squares);
-		this.squares.push(ref);
-	}
+    /**
+        If you ref a React component you need to use findDomNode to get the correct methods of DOM on ref.
+    */
     render(){
+        let tran = -1*this.state.scrollTop*0.5;
         return (
-            <div className="app">
-                <FadeBackground>
+            <div className="home">
+                <FadeBackground scrollTop={this.state.scrollTop}>
                     <Clouds>
-                        <div style={{transform:'translateY(' + this.state.yCanvas + 'px)'}} className={"app__header"} >
+                        <div style={{transform:'translateY(' + tran + 'px)'}} className={"home__header"} >
                             <h1>Alexander Morton</h1>
                             <h2>Web Developer</h2>
                         </div>
-                        <Squares isShow={this.state.isShow[0]} ref={(ref)=>{this.addRef.bind(this)(ref)}}>
+                        <div className={"home__gap"}/>
+                        <Squares isShow={this.state.isShow[0]} ref={(ref)=>{this.squares[0] = ReactDOM.findDOMNode(ref)}}>
                         </Squares>
-                        <Squares isShow={this.state.isShow[1]} ref={(ref)=>{this.addRef.bind(this)(ref)}}>
+                        <Squares isShow={this.state.isShow[1]} ref={(ref)=>{this.squares[1] = ReactDOM.findDOMNode(ref)}}>
                         </Squares>
-                        <Squares isShow={this.state.isShow[1]} ref={(ref)=>{this.addRef.bind(this)(ref)}}>
+                        <Squares isShow={this.state.isShow[1]} ref={(ref)=>{this.squares[2] = ReactDOM.findDOMNode(ref)}}>
                         </Squares>
-                        <footer> {this.props.title} </footer>
                     </Clouds>
                 </FadeBackground>
             </div>
--- src/home.test.js
+++ src/home.test.js
@@ -6,31 +6,31 @@ import FadeBackground from "./fadeBackground";
 import Squares from "./squares";
 import Clouds from "./clouds";
 import sinon from 'sinon';
-import App from './App';
+import Home from './Home';
 
 
-describe("<App/>", ()=>{
+describe("<Home/>", ()=>{
     it('renders without crashing', () => {
       const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
+      ReactDOM.render(<Home />, div);
     });
     it('renders correct number of components', () => {
         const wrapper = shallow(
-            <App/>
+            <Home/>
         );
         expect(wrapper.find(FadeBackground).length).toBe(1);
         expect(wrapper.find(Squares).length).toBe(3);
         expect(wrapper.find(Clouds).length).toBe(1);
     });
     it('correctly sets isShow to 0 before component did mount', () => {
-        const wrapper = mount(<App/>);
+        const wrapper = mount(<Home/>);
         expect(wrapper.state("isShow").length).toBe(0);
     });
     it('calls componentDidMount() lifecycle method', () => {
-        const componentDidMountSpy = sinon.spy(App.prototype, 'componentDidMount');
-        const wrapper = mount(<App />);
+        const componentDidMountSpy = sinon.spy(Home.prototype, 'componentDidMount');
+        const wrapper = mount(<Home />);
 
-        expect(App.prototype.componentDidMount.calledOnce).toBe(true);
+        expect(Home.prototype.componentDidMount.calledOnce).toBe(true);
 
         componentDidMountSpy.restore();
     });
