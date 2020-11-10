7455afad8dc1b8fd9c12d2369596776a2e133a0c
Alexander Morton <mortonprod@gmail.com>
2020-11-10 00:13:51 +0000

    Update sending message to use dialog element

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,84 @@
 // require("./splash-page/index");
 var $ = require('jquery');
-
-function submitToAPI(e) {
-  e.preventDefault();
-  var url = "https://ses.alexandermorton.co.uk/ses";
-
-   var Namere = /[A-Za-z]{1}[A-Za-z]/;
-   if (!Namere.test($("#Name").val())) {
-                   alert ("Name can not less than 2 char");
-       return;
-   }
-   if ($("#Email").val()=="") {
-       alert ("Please enter your email id");
-       return;
-   }
-   var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-   if (!reeamil.test($("#Email").val())) {
-       alert ("Please enter valid email address");
-       return;
-   }
-
-  var name = $("#Name").val();
-  var email = $("#Email").val();
-  var note = $("#note").val();
-  var data = {
-     name : name,
-     email : email,
-     note : note
-   };
-
-  $.ajax({
-    type: "POST",
-    url: url,
-    dataType: "json",
-    crossDomain: "true",
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(data),
-
-    
-    success: function () {
-      // clear form and show a success message
-      alert("Message Sent");
+import dialogPolyfill from 'dialog-polyfill'
+$(document).ready(function() {
+  var is_reset= false;
+  var dialog = document.querySelector('#dialog');
+  var dialog_title = dialog.getElementsByClassName("mdl-dialog__title")[0]
+  var dialog_paragraph = dialog.getElementsByClassName("mdl-dialog__content")[0].getElementsByTagName('p')[0]
+  dialogPolyfill.registerDialog(dialog);
+  dialog.querySelector('button:not([disabled])').addEventListener('click', function() {
+    dialog.close();
+    if(is_reset){
       document.getElementById("contact-form").reset();
-  location.reload();
-    },
-    error: function () {
-      // show an error message
-      alert("Something went wrong. Try contacting me through linkedin");
-    }});
-}
-
-var button_my_button = "#contact-form-button";
-$(button_my_button).click(function(e){
-  submitToAPI(e);
+      location.reload();
+      is_reset = false;
+    }
+  });
+  function submitToAPI(e) {
+    e.preventDefault();
+    var url = "https://ses.alexandermorton.co.uk/ses";
+     var Namere = /[A-Za-z]{1}[A-Za-z]/;
+     if (!Namere.test($("#Name").val())) {
+          dialog_title.innerHTML = "Fix Me"
+          dialog_paragraph.innerHTML = "Name must be more than 2 characters";
+          dialog.showModal();
+          return;
+     }
+     if ($("#Email").val()=="") {
+        dialog_title.innerHTML = "Fix Me"
+        dialog_paragraph.innerHTML = "Please enter your email";
+        dialog.showModal();
+        return;
+     }
+     var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+     if (!reeamil.test($("#Email").val())) {
+          dialog_title.innerHTML = "Fix Me"
+          dialog_paragraph.innerHTML = "Please enter valid email address";
+          dialog.showModal();
+         return;
+     }
+     if ($("#note").val()=="") {
+      dialog.showModal();
+      dialog_title.innerHTML = "Fix Me"
+      dialog_paragraph.innerHTML = "Please enter your message";
+      return;
+    }
+  
+    var name = $("#Name").val();
+    var email = $("#Email").val();
+    var note = $("#note").val();
+    var data = {
+       name : name,
+       email : email,
+       note : note
+     };
+  
+    $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      crossDomain: "true",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+  
+      
+      success: function () {
+        is_reset = true;
+        dialog.showModal();
+        dialog_title.innerHTML = "Sent"
+        dialog_paragraph.innerHTML = "I will try to get back to you as soon as possible.";
+        return;
+      },
+      error: function () {
+        dialog.showModal();
+        dialog_title.innerHTML = "Error"
+        dialog_paragraph.innerHTML = "Something went wrong on my side. Try contacting me through linkedin";
+        return;
+      }});
+  }
+  var button_my_button = "#contact-form-button";
+  $(button_my_button).click(function(e){
+    submitToAPI(e);
+  });
 });
